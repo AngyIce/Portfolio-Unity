5db2fc6f96c834ff475fd6d0c252b7a1b8408225
angyice <leonardo_villa@studenti.naba.it>
2021-05-14 14:25:27 +0200

    Initial commit

---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject[] spawnableObjects;
+    GameObject objectToSpawn;
+
+    private int xPos;
+    private int yPos;
+    private int zPos;
+    private int objectToGenerate;
+    private int objectQuantity = 500;
+
+    void Start()
+    {
+
+        for (int i = 0; i < objectQuantity; i++)
+        {
+            xPos = Random.Range(-200, 200);
+            yPos = Random.Range(-200, 200);
+            zPos = Random.Range(-250, 250);
+
+            objectToGenerate = Random.Range(0, spawnableObjects.Length);
+            objectToSpawn = spawnableObjects[objectToGenerate];
+
+            Instantiate(objectToSpawn, new Vector3(xPos, yPos, zPos), Quaternion.identity);
+
+        }
+
+    }
+
+}
+
+
