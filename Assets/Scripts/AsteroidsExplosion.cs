2c31c4d7af8eb3b99faf47b24e7165925b7d9778
angyice <leonardo_villa@studenti.naba.it>
2021-07-10 00:11:37 +0200

    Ha già vinto l'Inghilterra

--- Assets/Scripts/AsteroidsExplosion.cs
+++ Assets/Scripts/AsteroidsExplosion.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class AsteroidsExplosion : MonoBehaviour
 {
+    public GameObject spawnObject;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Projectile"))
         {
             gameObject.SetActive(false);
+            Instantiate(spawnObject, transform.position += Vector3.up * 5.0f, transform.rotation);
         }
     }
 }
